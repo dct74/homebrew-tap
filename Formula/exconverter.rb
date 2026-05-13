b4f457281fe1d7276c74736762ca4b7af8e0f3aa
dct74 <he_zhengqing@icloud.com>
2026-05-13 16:23:00 +0800

    feat: update formula download link

--- Formula/exconverter.rb
+++ Formula/exconverter.rb
@@ -1,7 +1,7 @@
 # watermark.rb
 class Exconverter < Formula
   desc "Convert excel to folders"
-  homepage "https://github.com/dct74/exconvert"
+  homepage "https://github.com/dct74/xlconvert"
   url "https://github.com/dct74/xlconvert/releases/download/v0.1/exconverter"
   sha256 "sha256:c15a5b199dca8d811fc4a0bbaf34885dff6a314f33e4f9d931c15a1024b2d5ed"
   version "0.1"
