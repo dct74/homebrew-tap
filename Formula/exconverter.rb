9ebd9ad2e51540213effafd6a9bc537ed758c37c
dct74 <he_zhengqing@icloud.com>
2026-05-15 13:55:43 +0800

    feat: update formula download link

--- Formula/exconverter.rb
+++ Formula/exconverter.rb
@@ -4,7 +4,7 @@ class Exconverter < Formula
   homepage "https://github.com/dct74/xlconvert"
   url "https://github.com/dct74/xlconvert/releases/download/v0.2/exconverter"
   sha256 "a78ba777d3780675bfc8d8cf79104e5c1ed786d05a8c3947a8619c90c834cd12"
-  version "0.1"
+  version "0.2"
   
   def install
     bin.install "exconverter" => "xlconv"
