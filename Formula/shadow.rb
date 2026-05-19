b7da6e5064962af25095b8cc0026b1f2be24e0b7
dct74 <he_zhengqing@icloud.com>
2026-05-20 01:24:01 +0800

    feat: update new version link

--- Formula/shadow.rb
+++ Formula/shadow.rb
@@ -2,9 +2,9 @@
 class Shadow < Formula
   desc "Batch add shadow to images"
   homepage "https://github.com/dct74/shadow"
-  url "https://github.com/dct74/shadow/releases/download/v0.2/shadow"
-  sha256 "8e083c5c8ded6ebcfc35f2ccfd580941ff526d6366cfe277c87c36f074ac561c"
-  version "0.2"
+  url "https://github.com/dct74/shadow/releases/download/v0.3/shadow"
+  sha256 "4e29e203461443858bf8e28c68c47260724646769e901201ee7c74ad7b9bb2fc"
+  version "0.3"
   
   def install
     bin.install "shadow"
