bb0d9af3101437b41d95034b36bcd5ab69755191
dct74 <he_zhengqing@icloud.com>
2026-05-22 13:40:32 +0800

    feat: optimize shadow effects

--- Formula/shadow.rb
+++ Formula/shadow.rb
@@ -2,9 +2,9 @@
 class Shadow < Formula
   desc "Batch add shadow to images"
   homepage "https://github.com/dct74/shadow"
-  url "https://github.com/dct74/shadow/releases/download/v0.3/shadow"
-  sha256 "4e29e203461443858bf8e28c68c47260724646769e901201ee7c74ad7b9bb2fc"
-  version "0.3"
+  url "https://github.com/dct74/shadow/releases/download/v0.4/shadow"
+  sha256 "1dbc61c0571e1b9e2db39ffaa8e2d902684a00b2900de5fbe1d3d8d27854eb9e"
+  version "0.4"
   
   def install
     bin.install "shadow"
