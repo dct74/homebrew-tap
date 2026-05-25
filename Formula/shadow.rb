927c3677c700168d75fa1995ece5c8ad9dda9b3e
dct74 <he_zhengqing@icloud.com>
2026-05-25 14:18:47 +0800

    feat: add parallel processing

--- Formula/shadow.rb
+++ Formula/shadow.rb
@@ -2,9 +2,9 @@
 class Shadow < Formula
   desc "Batch add shadow to images"
   homepage "https://github.com/dct74/shadow"
-  url "https://github.com/dct74/shadow/releases/download/v0.4/shadow"
-  sha256 "1dbc61c0571e1b9e2db39ffaa8e2d902684a00b2900de5fbe1d3d8d27854eb9e"
-  version "0.4"
+  url "https://github.com/dct74/shadow/releases/download/v0.5/shadow"
+  sha256 "ec977fc6850da25759ea3f5db3d1fb4f4be43ef6fdda436e0214b8883e2b4e5e"
+  version "0.5"
   
   def install
     bin.install "shadow"
