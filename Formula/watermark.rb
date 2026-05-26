9b955489993b004cbfae9325e9c0fd14411e3e00
dct74 <he_zhengqing@icloud.com>
2026-05-26 19:07:43 +0800

    feat: fix bugs

--- Formula/watermark.rb
+++ Formula/watermark.rb
@@ -2,9 +2,9 @@
 class Watermark < Formula
   desc "批量图片和PDF文件添加水印"
   homepage "https://github.com/dct74/watermark"
-  url "https://github.com/dct74/watermark/releases/download/v0.1/watermark"
-  sha256 "c850fdc2641eeeae2ca5b8f3858a6a7cb2721b645bde71827ae0916151af8b8d"
-  version "0.1"
+  url "https://github.com/dct74/watermark/releases/download/v0.2/watermark"
+  sha256 "72d30b101a9e0dc9a839ccaf33fdaa8f1d2607e3f15f8185b4cd2bb4e9e92068"
+  version "0.2"
   
   def install
     bin.install "watermark"
