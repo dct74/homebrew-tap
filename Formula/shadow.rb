765eb78233948f1392092d5486834561ed154e2a
dct74 <he_zhengqing@icloud.com>
2026-07-29 19:07:18 +0800

    fix bugs

--- Formula/shadow.rb
+++ Formula/shadow.rb
@@ -2,9 +2,9 @@
 class Shadow < Formula
   desc "Batch add shadow to images"
   homepage "https://github.com/dct74/shadow"
-  url "https://github.com/dct74/shadow/releases/download/v0.6/shadow"
-  sha256 "125ef4feff23e5a557596dc97154d2f4b1eda8ac520c6239429ddb4ab0e16471"
-  version "0.6"
+  url "https://github.com/dct74/shadow/releases/download/v0.7/shadow"
+  sha256 "c31be3a29f31902d0a9b01c933a0acfd51dc0e80ee34e3cf547392ee1cb5c0a0"
+  version "0.7"
   
   def install
     bin.install "shadow"
