4eacd6c231f4f6032770a6f867fddbf9deb0c6ab
dct74 <he_zhengqing@icloud.com>
2026-09-08 21:09:54 +0800

    fix bug

--- Formula/exconverter.rb
+++ Formula/exconverter.rb
@@ -2,12 +2,12 @@
 class Exconverter < Formula
   desc "Convert excel to folders"
   homepage "https://github.com/dct74/xlconvert"
-  url "https://github.com/dct74/xlconvert/releases/download/v0.2/exconverter"
-  sha256 "a78ba777d3780675bfc8d8cf79104e5c1ed786d05a8c3947a8619c90c834cd12"
+  url "https://github.com/dct74/xlconvert/releases/download/v0.2/xlconv"
+  sha256 "8f588221304c44acf935f2a1a6760687f82839a61a51566f9a99aa1ca8f0c99c"
   version "0.2"
   
   def install
-    bin.install "exconverter" => "xlconv"
+    bin.install "xlconv"
   end
 
   test do
