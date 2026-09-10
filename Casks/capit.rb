32081745648949de2b78e71d58ba2fab127519e3
dct74 <he_zhengqing@icloud.com>
2026-09-10 20:23:37 +0800

    fix bug

--- Casks/capit.rb
+++ Casks/capit.rb
@@ -1,11 +1,11 @@
 cask "capit" do
-  version "v0.2"
-  sha256 "6f25ab8fb4dde597a01063b5402845376bfb23595c687ff36adffde9f8fdc0b7"
-  url "https://github.com/dct74/capit/releases/download/v0.2/Capit.app.zip"
+  version "v0.3"
+  sha256 "404f6d3c776f52a188479d5d146622b454aa8d3d0cfca9841b5fdada9b2892f5"
+  url "https://github.com/dct74/capit/releases/download/v0.3/Capit.app.zip"
   name "Capit"
   desc "Minimalist macOS native screenshot tool"
   homepage "https://github.com/dct74/capit"
 
-  # 核心安装指令
+  # install command
   app "capit.app"
 end
